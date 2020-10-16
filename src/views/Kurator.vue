7e0588d22a6340238834c7a6a55fbf933c40f537
hovenone <erdeiandris9@gmail.com>
2020-10-16 19:06:04 +0200

    david vagyok csőcső

--- src/views/Kurator.vue
+++ src/views/Kurator.vue
@@ -1,17 +1,84 @@
 <template>
   <div>
     <Nav />
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="container">
       <div class="row">
-        <div class="col-md-4">
-          <img src="../assets/2.jpg" class="card-img" alt="..." />
+        <div class="col">
+          <img src="../assets/kurator1.jpg" class="card-img" alt="..." />
         </div>
-        <div class="col-md-8">
+        <div class="col">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"></p>
+            <h5 class="card-title">Sinkó István (1951-)</h5>
             <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
+              Képzőművész, művészeti író, tanár 34 évig általános iskolában
+              tanított rajzot, művészettörténetet, majd 2006-2020 –ig a Képző-és
+              Iparművészeti Szakgimnázium, rajz és festőtanára (a festő szak
+              vezetője). Szakköröket vezet, szakmai lapokban publikál 30 éve.
+              Képzőművészként 41 önálló kiállítása volt hazai és külföldi
+              kiállítótermekben. Munkái megtalálhatóak magyarországi és külföldi
+              múzeumokban és magángyűjteményekben.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img src="../assets/kurator2.jpg" class="card-img" alt="..." />
+        </div>
+        <div class="col">
+          <div class="card-body">
+            <h5 class="card-title">Makói Juhász István (1962-)</h5>
+            <p class="card-text">
+              Jelenleg a Magyar Képzőművészeti Egyetem gyakorló iskolájában a
+              Kisképzőben dolgozom festés-technika és rajz tanárként a
+              festőszakon. Korábban a Magyar Nemzeti Galéria Régi Magyar
+              Osztályának restaurátor műhelyében voltam osztályvezető
+              főrestaurátor 23 éven keresztül. A restaurálás mellett, festek,
+              alkotok, fotózom, és grafikákat is készítek. Munkáimat kiállítások
+              keretén belül mutatom be a nagyközönség számára. Tagja vagyok a
+              Magyar Alkotóművészek- és a Magyar Restaurátorok Országos
+              Egyesületének.
+            </p>
+
+            <p><a href="https://makoi-juhasz.eoldal.hu/">Weboldal</a></p>
+            <p>
+              <a
+                href="http://archiv.restauratorkamara.hu/portfolio/portfolio.php?show_user_id=577"
+                >Restaurátor kamara</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img src="../assets/kurator3.jpeg" class="card-img" alt="..." />
+          <img src="../assets/kurator4.jpg" class="card-img" alt="..." />
+        </div>
+        <div class="col">
+          <div class="card-body">
+            <h5 class="card-title">Veres-Vancsai Lilla (1996-)</h5>
+            <p class="card-text">
+              A művészet mindig is a lételemem volt. Kisgyermekkorom óta festek,
+              rajzolok, és azon szerencsések közé tartozom, akik ebben az
+              irányban tudtak továbbtanulni. 2019-ben végeztem a Kaposvári
+              Egyetem Rippl-Rónai Művészeti Karán festő szakon. Egyetemi
+              tanulmányaim mellett már dolgoztam a szakmai tapasztalat
+              megszerzésén, így 2017 óta szervezem a Vad Art rendezvényeket,
+              különböző kiállításokat, workshopokat. A rendezvények adottságából
+              kifolyólag már a kezdetektől meghatározó feladatom a zsűrizés és a
+              kurátori munka. Minden pályázat után izgalmas alkotások sokasága
+              tárul elénk, ebből pedig nem egyszerű feladat egy harmonikus,
+              mégis sokszínű kiállítást megvalósítani. Az elmúlt évek során
+              egyre tapasztaltabb lettem csapattársaimmal együtt és úgy gondolom
+              változatos tárlatokat hoztunk létre. A Vad Art főszervezőjeként
+              már számos rendezvényt szerveztem, számomra a megvalósulásuk egy
+              igazi mámoros pillanat. Felemelő élmény látni, ahogy a programok,
+              a hangulat és a szellemiség együtt munkálkodik, üzenetet közvetít
+              és a résztvevők kapcsolódni tudnak mindehhez. A művészeti
+              rendezvények szervezése ugyanolyan alkotási folyamat számomra,
+              mintha egy képet festenék, csupán ehhez kell egy szuper csapat is,
+              akikkel öröm együtt dolgozni.
             </p>
           </div>
         </div>
@@ -31,4 +98,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+.col {
+  height: 100%;
+  vertical-align: middle;
+}
+.card-img {
+  margin-bottom: 40px;
+}
 </style>
